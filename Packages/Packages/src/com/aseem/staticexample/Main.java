605c5166e039f7c8d19e83af2f330e2b8625daf6
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-04-22 01:49:51 +0530

    new Packages, static non static, singleton classes

--- Packages/Packages/src/com/aseem/staticexample/Main.java
+++ Packages/Packages/src/com/aseem/staticexample/Main.java
@@ -4,6 +4,6 @@ public class Main {
 //        if the files are the same folder u don't have to import them
         human aseem = new human(21, "Aseem", 6, 50000);
         human aryan = new human(20, "aryan", 6, 1000000);
-
+        System.out.println(human.population);
     }
 }
--- Packages/Packages/src/com/aseem/staticexample/human.java
+++ Packages/Packages/src/com/aseem/staticexample/human.java
@@ -6,10 +6,14 @@ public class human {
     int height;
     int salary;
 
+//    static variable is not linked to particular object but to the whole class
+    static int population = 0;
+
     public human(int age, String name, int height, int salary){
         this.age = age;
         this.name = name;
         this.height = height;
         this.salary = salary;
+        human.population++;
     }
 }
