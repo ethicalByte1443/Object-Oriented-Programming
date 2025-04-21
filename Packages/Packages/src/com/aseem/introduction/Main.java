7feab10790454a92a1dcf3ec7a44df5b1f47dc5a
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-04-21 23:53:19 +0530

    new Packages, static non static, singleton classes

---
+++ Packages/Packages/src/com/aseem/introduction/Main.java
@@ -0,0 +1,10 @@
+package com.aseem.introduction;
+
+import static com.aseem.packages.greetings.message;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        message();
+    }
+}
---
+++ Packages/Packages/src/com/aseem/packages/greetings.java
@@ -0,0 +1,10 @@
+package com.aseem.packages;
+
+public class greetings {
+    public static void main(String[] args) {
+
+    }
+    public static void message(){
+        System.out.println("greetings");
+    }
+}
