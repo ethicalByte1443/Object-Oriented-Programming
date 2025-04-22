9cfe151a27faf3e4d0255e58ffa9001c1e817790
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-04-22 20:36:07 +0530

    new insights

--- Packages/Packages/src/com/aseem/staticexample/Main.java
+++ Packages/Packages/src/com/aseem/staticexample/Main.java
@@ -4,7 +4,18 @@ public class Main {
 //        if the files are the same folder u don't have to import them
         human aseem = new human(21, "Aseem", 6, 50000);
         human aryan = new human(20, "aryan", 6, 1000000);
+
+
+//        this is the wrong practise
+        System.out.println(aryan.population);
+        System.out.println(aseem.population);
+
+//        the correct convention is this using <classname>.population
         System.out.println(human.population);
+
+
+
+
         System.out.println("**************");
     }
 }
