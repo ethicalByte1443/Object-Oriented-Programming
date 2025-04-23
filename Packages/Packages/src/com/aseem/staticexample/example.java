44f3f7c81b8285094bb4994a5e3a9c8cd24006a5
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-04-23 23:19:28 +0530

    non-static declaration

---
+++ Packages/Packages/src/com/aseem/staticexample/example.java
@@ -0,0 +1,20 @@
+package com.aseem.staticexample;
+
+public class example
+{
+//    if the 9 line is kept here u will find the line number 16 will print 3
+    public static  example RED = new example();
+    public static final example BLUE = new example();
+    public static final example GREEN = new example();
+    static int i=0;                 // if this line is kept here the line number 16 will always print 0 because static instances are loaded as per the same order they are listed in the class so the last value of i is 0
+
+    example(){
+        System.out.println("Incrementing 'i'  here  "+(++i));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The 'i' variable is not incremented here, it still shows '0' ,  'i' is: " + i);  // line 14
+//        example x = new example();
+//        System.out.println(x.i);
+    }
+}
--- Packages/Packages/src/com/aseem/staticexample/nonstatic.java
+++ Packages/Packages/src/com/aseem/staticexample/nonstatic.java
@@ -4,7 +4,19 @@ public class nonstatic {
     int a = 10;
     int b = 20;
 
+
+    static {
+        System.out.println("Static box initiated");
+        nonstatic obj = new nonstatic();
+        obj.a = 100;
+        obj.b = 200;
+    }
     public static void main(String[] args) {
-        System.out.println(a);      // this will give the error
+//        System.out.println(a);      // this will give the error
+
+
+        nonstatic obj = new nonstatic();
+        obj.a = 100;
+        System.out.println(obj.a);
     }
 }
