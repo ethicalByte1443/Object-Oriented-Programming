61b7d9af1b0078c090dce75aed47243ed0f3ce7a
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-04-30 11:45:42 +0530

    Learning inheritance

---
+++ inheritanceAndPolymorphism/com/aseem/Inheritance/Main.java
@@ -0,0 +1,7 @@
+package com.aseem.inheritanceAndPolymorphism.com.aseem.Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ inheritanceAndPolymorphism/com/aseem/Inheritance/box.java
@@ -0,0 +1,35 @@
+package com.aseem.inheritanceAndPolymorphism.com.aseem.Inheritance;
+
+public class box {
+    double l;
+    double b;
+    double h;
+
+    box () {
+        this.l = -1;
+        this.b = -1;
+        this.h = -1;
+    }
+
+//    cuboid
+    box (double l, double b, double h){
+        System.out.println("Cuboid");
+        this.l = l;
+        this.b = b;
+        this.h = h;
+    }
+
+//    cube
+    box (double side){
+        System.out.println("Cube");
+        this.l = side;
+        this.b = side;
+        this.h = side;
+    }
+
+    box (box old){
+        this.l = old.l;
+        this.b = old.b;
+        this.h = old.h;
+    }
+}
