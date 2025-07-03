f33b3c950dc67a0d6899e8a4038260f1b054cdd9
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-07-03 13:58:15 +0530

    static vs instance

--- Training/Employee.java
+++ Training/Employee.java
@@ -4,16 +4,27 @@ public class Employee {
     int salary;
     int Empcounter = 0;
     static int count = 0;
+    static String companyName = "AIAI";
+
     Employee(String name, int salary){
         this.id = ++count;
         this.name = name;
         this.salary = salary;
     }
 
+    public void change_company_name(String new_name){
+        companyName = new_name;
+    }
+
+    public static void get_emp_count(){
+        System.out.println(count);
+    }
+
     public void display(){
         System.out.println(this.id);
         System.out.println(this.name);
         System.out.println(this.salary);
+        System.out.println(companyName);
     }
 
 
@@ -23,6 +34,9 @@ public class Employee {
 
         employee1.display();
         employee2.display();
+
+
+        get_emp_count();
 
 
     }
