8c9a51452efbc9ce3a76325af6be4df3ca1d29a6
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-07-08 16:46:22 +0530

    new content

--- Training/stringPractice/test1.java
+++ Training/stringPractice/test1.java
@@ -7,12 +7,10 @@ interface B{
 }
 
 public class test1 implements A, B{
-    @Override
     public void display() {
         System.out.println("Display from B");
     }
 
-    @Override
     public void show() {
         System.out.println("Show from A");
     }
