6982b20c418e676d7e624c90b704697dc2d17798
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-07-08 16:48:45 +0530

    new content

--- Training/stringPractice/test1.java
+++ Training/stringPractice/test1.java
@@ -5,13 +5,20 @@ interface A{
 interface B{
     void display();
 }
+interface C{
+    void print();
+}
 
-public class test1 implements A, B{
+public class test1 implements A, B, C{
     public void display() {
         System.out.println("Display from B");
     }
 
     public void show() {
         System.out.println("Show from A");
+    }
+
+    public void print(){
+        System.out.println("Print form C");
     }
 }
