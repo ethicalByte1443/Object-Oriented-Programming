0fb8ce4e79337bfbf9b76ef03e6a543224e9295a
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-07-08 15:38:45 +0530

    new content

--- Training/Inheritance/Main.java
+++ Training/Inheritance/Main.java
@@ -1,10 +1,13 @@
 package Inheritance;
 
 public class Main{
-
+    public static double area_rectangle(shape rectangle){
+        return rectangle.length * rectangle.breadth;
+    }
     public static void main(String[] args) {
-        shapeColor box1 = new shapeColor(20, 30, "Yellow");
-        box1.display();
+        shape rectangle = new shape(2,4);
+        double area = area_rectangle(rectangle);
+        System.out.println("Area : "+area);
     }
 
 }
